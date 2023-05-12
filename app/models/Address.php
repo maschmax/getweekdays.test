087306375da4e4a682d0be7d11659260bd336eb1
maria.schon <maria@mariaschon.se>
2023-05-12 08:58:13 +0200

    [ENHANCEMENT] Adding Address setStreet

--- app/models/Address.php
+++ app/models/Address.php
@@ -4,8 +4,26 @@ namespace Masch\Getweekdays\models;
 
 class Address
 {
+    /**
+     * @return mixed
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * @param mixed $street
+     */
+    public function setStreet($street): void
+    {
+        $this->street = $street;
+    }
+    private $street;
     public function __construct(){
 
     }
+
+
 
 }
