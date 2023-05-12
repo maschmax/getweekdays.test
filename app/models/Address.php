27403f97be3ba8182c351712e04f4849eeff948f
maria.schon <maria@mariaschon.se>
2023-05-12 10:25:30 +0200

    [ENHANCEMENT] Adding City to Address.

--- app/models/Address.php
+++ app/models/Address.php
@@ -4,6 +4,23 @@ namespace Masch\Getweekdays\models;
 
 class Address
 {
+    private $city;
+
+    /**
+     * @return mixed
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param mixed $city
+     */
+    public function setCity($city): void
+    {
+        $this->city = $city;
+    }
     public function __construct(){
 
     }
